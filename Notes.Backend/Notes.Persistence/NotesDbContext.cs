4ed7f23616ce09614821feef264ed9ed56ac5c9c
Александр Зенякин <a.zeniakin@bars.group>
2021-12-30 15:18:01 +0300

    Mapping and DB Commands

---
+++ Notes.Backend/Notes.Persistence/NotesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Notes.Persistence.EntityTypeConf;
+
+namespace Notes.Persistence
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        public NotesDbContext(DbContextOptions<NotesDbContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new NoteConf());
+            base.OnModelCreating(builder);
+        }
+    }
+}
